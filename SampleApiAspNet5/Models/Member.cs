ab79126a77e5c7bc090041daad9d3d5fbbcfd4f0
hideru <jota.16.stigma.world@gmail.com>
2021-07-18 01:15:54 +0900

    first commit

---
+++ SampleApiAspNet5/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleApiAspNet5.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
